a36a2191f23c16d3ac1019234e33fe1ce2a7621e
DyamoSayajin <98530467+DyamoSayajin@users.noreply.github.com>
2022-02-17 14:00:31 +0100

    Ajout fonction liste_videos
    La fonction liste_videos dans le fichier dico_video récupère la liste des fichiers videos dans le dossiers Videos. Elle retourne une liste de dictionnaire contentant le nom et les particularités du fichier en question. Le fichier views prend cette liste et l'intègre à un autre dictionnaire qui est utilisé en argument dans la fonction acceuil.

--- ServeurVOD/VOD/dico_video.py
+++ ServeurVOD/VOD/dico_video.py
@@ -1,18 +1,20 @@
 import os
+import cv2
+
 
 def liste_videos():
-    liste_fichier= os.listdir(r'D:\Users\Noa\Documents\GitHub\VOD-Project\ServeurVOD\Videos') #Liste des fichiers présent, avec extensions.
-    liste_nom = []
+    liste = []
+    liste_fichier= os.listdir("d:/Users/Noa/Documents/GitHub/VOD-Project/ServeurVOD/Videos") #Liste des fichiers présent, avec extensions.
     for fichier in liste_fichier:
+        file_path = os.path.join("d:/Users/Noa/Documents/GitHub/VOD-Project/ServeurVOD/Videos/",fichier)
+        vid = cv2.VideoCapture(file_path)
+        height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
         nom_video = ''.join(fichier.split('.')[:-1]) #Séparation de l'extension et du nom.
-        liste_nom.append(nom_video)   #Ajout du nom sans l'extension à la nouvelle liste.
-    return liste_nom
-
-print(liste_videos())
-    
-"""import cv2
-file_path = "./video.avi"
-vid = cv2.VideoCapture(file_path)
-height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
-width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
-"""
+        dictionnaire = {
+            'nom':nom_video,
+            'hauteur':height,
+            'largeur':width
+        }
+        liste.append(dictionnaire)
+    return liste
--- ServeurVOD/VOD/views.py
+++ ServeurVOD/VOD/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from VOD.dico_video import liste_videos
 
 
 #Dictionnaire contenant le titre et la description de chaque fichier dans le dossier vidéos.Elle sera renvoyé par une fonction dans un autre fichier python.
-titre = {}
+titre = {
+    'titre':'BONJOUR',
+    'soustitres':'COMME CA VA VA ?',
+    'videos': liste_videos()
+
+}
 
 def acceuil(request):    #Retourne la page html de base. La fonction prend en argument le dictionnaire pour que la page html affiche la liste des vidéos disponibles.
     return render (request,'index.html',titre)
